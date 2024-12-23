78e4fd7344fff91f8b041e1cc17cd48ad49ef173
Ethan721au <stephane.mercier.au@gmail.com>
2024-12-24 10:04:00 +1100

    change

--- src/lib/Input/styled.ts
+++ src/lib/Input/styled.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-svelte5';
 
-export const Wrapper = styled<{ type: string }>(
+export const Wrapper = styled<{ type?: string }>(
 	'div',
 	({ type }) => `
   display: flex;
@@ -13,5 +13,30 @@ export const Wrapper = styled<{ type: string }>(
    & > input {
     order: ${type === 'checkbox' ? 0 : 1};
   }
+  `
+);
+
+export const InputField = styled<{ type?: string }>(
+	'input',
+	({ type }) => `
+  appearance: none;
+  outline: none;
+  border-radius: 4px;
+  border: 1px solid #e5e5e5;
+
+  width: ${type === 'checkbox' ? '16px' : '100%'};
+  height: ${type === 'checkbox' ? '16px' : '100%'};
+  cursor: ${type === 'checkbox' ? 'pointer' : 'text'};
+
+    &:checked {
+    background-color: var(--primary-peach);
+    border: none;
+    }
+
+    &:focus {
+    transition: ${type === 'checkbox' ? 'none' : 'outline-offset 0.2s ease-out'};
+    outline: ${type === 'checkbox' ? 'none' : 'var(--focused-base-outline)'};
+    outline-offset: ${type === 'checkbox' ? 'none' : 'var(--focused-base-outline-offset)'};
+    }
   `
 );
